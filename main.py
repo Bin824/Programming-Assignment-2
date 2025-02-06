403f5944b07d75ea8e36896b083da1ea6bc93d48
Bin824 <zomberrylol@gmail.com>
2025-02-06 15:05:50 -0500

    done

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Name: Bin Ni
+Email: bin.ni81@myhunter.cuny.edu
+Resources: None
+"""
+
+def is_leap():
+    """
+    This function determines whether an integer between 1000 and 10,000 is a leap year
+    """
+    value = int(input("Enter an integer between 1,000 and 10,000: "))
+
+    return (value % 4 == 0 and value % 100 != 0) or value % 400 == 0
+
+print(is_leap())
